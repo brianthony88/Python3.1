c4261d2438a39926d9c48bd9da2c4ed95ca07f1a
Brian Cox <brianthony.cox88.com>
2023-12-22 16:17:17 -0600

    first commit

---
+++ my_module.py
@@ -0,0 +1,9 @@
+# my_module.py
+
+def calculate_square_footage(length, width):
+    area = length * width
+    return area
+
+def calculate_circumference(radius):
+    circumference = 2 * 3.14 * radius
+    return circumference
